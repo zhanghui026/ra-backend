77d12ce4780cd51362b15b0555e0a44bfb9748be
zhanghui <zhanghui026@gmail.com>
2020-04-02 04:42:05 +0800

    add mock service

--- src/main/java/com/zh/raback/service/MockService.java
+++ src/main/java/com/zh/raback/service/MockService.java
@@ -31,41 +31,44 @@ public class MockService {
 
     @PostConstruct
     public void init (){
-
-        List<String> categories = Lists.newArrayList("animals",
-            "beard","business","cars","city",
-            "flowers","food","nature","people","sports","tech","travel","water");
-        List<Category> cats = categories.stream().map(it -> {
-            Category category = new Category();
-            category.setName(it);
-            return category;
-        }).collect(Collectors.toList());
-        categoryRepository.saveAll(cats);
-
-        List<Product> products = cats.stream().map(it -> {
-            return IntStream.range(0, 10).mapToObj(index -> {
-                int width = CommonUtils.toRandomInt(10, 40);
-                int height = CommonUtils.toRandomInt(10, 40);
-                int price = CommonUtils.toRandomInt(5,20);
-                String thumbnail = "https://marmelab.com/posters/" + it.getName() + "-" + (index + 1) + ".jpeg";
-                String image = "https://marmelab.com/posters/" + it.getName() + "-" + (index + 1) + ".jpeg";
-                Product product = new Product();
-                char c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()[Faker.instance().random().nextInt(26)];
-                product.categoryId(it.getId())
-                    .reference(it.getName().substring(0,2)+
-                        "-"+RandomStringUtils.randomAlphabetic(5)+
-                        "-" +c
-                     ).width((long)width)
-                    .height((long)height)
-                    .price((long)price)
-                    .thumbnail(thumbnail)
-                    .image(image)
-                    .description((Faker.instance().lorem().paragraph()))
-                    .stock((long)CommonUtils.toRandomInt(0,250));
-                return product;
+        if (categoryRepository.count() == 0) {
+            List<String> categories = Lists.newArrayList("animals",
+                "beard", "business", "cars", "city",
+                "flowers", "food", "nature", "people", "sports", "tech", "travel", "water");
+            List<Category> cats = categories.stream().map(it -> {
+                Category category = new Category();
+                category.setName(it);
+                return category;
             }).collect(Collectors.toList());
-        }).flatMap(List::stream).collect(Collectors.toList());
+            categoryRepository.saveAll(cats);
+        }
+        if (categoryRepository.count() == 0) {
+            List<Category> cats = categoryRepository.findAll();
+            List<Product> products = cats.stream().map(it -> {
+                return IntStream.range(0, 10).mapToObj(index -> {
+                    int width = CommonUtils.toRandomInt(10, 40);
+                    int height = CommonUtils.toRandomInt(10, 40);
+                    int price = CommonUtils.toRandomInt(5, 20);
+                    String thumbnail = "https://marmelab.com/posters/" + it.getName() + "-" + (index + 1) + ".jpeg";
+                    String image = "https://marmelab.com/posters/" + it.getName() + "-" + (index + 1) + ".jpeg";
+                    Product product = new Product();
+                    char c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()[Faker.instance().random().nextInt(26)];
+                    product.categoryId(it.getId())
+                        .reference(it.getName().substring(0, 2) +
+                            "-" + RandomStringUtils.randomAlphabetic(5) +
+                            "-" + c
+                        ).width((long) width)
+                        .height((long) height)
+                        .price((long) price)
+                        .thumbnail(thumbnail)
+                        .image(image)
+                        .description((Faker.instance().lorem().paragraph()))
+                        .stock((long) CommonUtils.toRandomInt(0, 250));
+                    return product;
+                }).collect(Collectors.toList());
+            }).flatMap(List::stream).collect(Collectors.toList());
 
-        productRepository.saveAll(products);
+            productRepository.saveAll(products);
+        }
     }
 }
